8b5ffb8393e901336be8eaaa2c0b6c231d91af43
Julien C. <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2026-04-18 00:47:00 +0200

    [INT] Init solution

---
+++ src/FlashBeat.Maui/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FlashBeat.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "FlashBeat.Maui" };
+        }
+    }
+}
