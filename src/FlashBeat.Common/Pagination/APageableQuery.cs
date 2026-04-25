bcf1458f8dcd3691f795fea9f6adf521b2b5f3b6
Julien C. <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2026-04-25 19:06:04 +0200

    Common - Add pagination models

---
+++ src/FlashBeat.Common/Pagination/APageableQuery.cs
@@ -0,0 +1,25 @@
+namespace FlashBeat.Common.Pagination;
+
+/// <summary>
+/// Class which represents a query to retrieve a page of items.
+/// </summary>
+public class PageableQuery
+{
+    public static readonly PageableQuery Default = new();
+    public static readonly PageableQuery NotPaginated = new() { IsPaginated = false };
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the query should retrieve a paginated result or not.
+    /// </summary>
+    public bool IsPaginated { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the start index.
+    /// </summary>
+    public int StartIndex { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the number of items to include on each page of results.
+    /// </summary>
+    public int PageSize { get; set; } = 10;
+}
---
+++ src/FlashBeat.Common/Pagination/PageResult.cs
@@ -0,0 +1,28 @@
+namespace FlashBeat.Common.Pagination;
+
+/// <summary>
+/// Represents a single page of results from a paginated query.
+/// </summary>
+/// <typeparam name="TResult">The type of the items contained in the page.</typeparam>
+public class PageResult<TResult>
+{
+    /// <summary>
+    /// Gets or sets the collection of items of the page.
+    /// </summary>
+    public TResult[] Items { get; set; } = [];
+
+    /// <summary>
+    /// Gets or sets the total number of items.
+    /// </summary>
+    public int TotalCount { get; set; }
+
+    /// <summary>
+    /// Method to create a new instance of <see cref="PageResult{TSelect}"/> with the items transformed by the provided
+    /// selector function.
+    /// </summary>
+    /// <typeparam name="TSelect">The type of the items in the resulting page.</typeparam>
+    /// <param name="selector">The select function.</param>
+    /// <returns>A new <see cref="PageResult{TSelect}"/> with the transformed items.</returns>
+    public PageResult<TSelect> SelectItems<TSelect>(Func<TResult, TSelect> selector)
+        => new() { TotalCount = this.TotalCount, Items = [.. this.Items.Select(selector)] } ;
+}
